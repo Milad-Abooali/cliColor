0f445da284b6b873f2be4d743d91f7e209e08ebf
Milad Abooali <m.abooali@hotmail.com>
2021-04-06 10:12:23 +0300

    Update print-all.php

--- example/print-all.php
+++ example/print-all.php
@@ -1,23 +1,18 @@
 <?php
 
-		$colors = new Colors();
-		$fgs = $colors->getForegroundColors();
-		$bgs = $colors->getBackgroundColors();
+	$colors = new Colors();
+	$fgs = $colors->getfgColors() {();
+	$bgs = $colors->getBgColors() {();
 
-		$count = count($fgs);
-		for ($i = 0; $i < $count; $i++) {
-			echo $colors->getColoredString("Test Foreground colors", $fgs[$i]) . "\t";
-			if (isset($bgs[$i])) {
-				echo $colors->getColoredString("Test Background colors", null, $bgs[$i]);
-			}
-			echo "\n";
-		}
+	$count = count($fgs);
+	for ($i = 0; $i < $count; $i++) {
+		echo $colors->setColor("Test Foreground colors", $fgs[$i]) . "\t";
+		if (isset($bgs[$i])) echo $colors->setColor("Test Background colors", null, $bgs[$i]);
 		echo "\n";
+	}
+	echo "\n";
 
-		// Loop through all foreground and background colors
-		foreach ($fgs as $fg) {
-			foreach ($bgs as $bg) {
-				echo $colors->getColoredString("Test Colors", $fg, $bg) . "\t";
-			}
-			echo "\n";
-		}
+	foreach ($fgs as $fg) {
+		foreach ($bgs as $bg) echo $colors->setColor("Test Colors", $fg, $bg) . "\t";
+		echo "\n";
+	}
